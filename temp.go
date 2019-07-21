34e9ba4f3a31c52e212bf24f9118a7f2b88e8832
Stuuu <stuartschrick@gmail.com>
2019-07-21 15:53:48 -0400

    type convert

--- temp.go
+++ temp.go
@@ -4,11 +4,12 @@ import "fmt"
 
 func main() {
 
-	var a int = 22;
-
-	a = 5423490234
-
-	fmt.Println(a);
+	var wholeNumber int = 1
+	var fractionalNumber float64 = 2.75
+	var wholeNumber2 int = int(fractionalNumber) // ERROR
+	var fractionalNumber2 float64 = float64(wholeNumber) // ERROR
+	fmt.Println("wholeNumber2:", wholeNumber2)
+	fmt.Println("fractionalNubmer2:", fractionalNumber2)
 
 
 }
