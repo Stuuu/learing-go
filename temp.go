ed31cde9e2a2ea4a1d4d3345a7a83ebc89037147
Stuuu <stuartschrick@gmail.com>
2019-07-21 15:59:11 -0400

    var scope

--- temp.go
+++ temp.go
@@ -2,14 +2,17 @@ package main
 
 import "fmt"
 
+var a = "a"
+
 func main() {
-
-	var wholeNumber int = 1
-	var fractionalNumber float64 = 2.75
-	var wholeNumber2 int = int(fractionalNumber) // ERROR
-	var fractionalNumber2 float64 = float64(wholeNumber) // ERROR
-	fmt.Println("wholeNumber2:", wholeNumber2)
-	fmt.Println("fractionalNubmer2:", fractionalNumber2)
-
-
+	var b = "b"
+	{
+		var c = "c"
+		{ 
+		var d = "d"
+			fmt.Println(a, b, c, d)
+		}
+		fmt.Println(a, b, c, d)
+	}
+	fmt.Println(a, b, c, d)
 }
